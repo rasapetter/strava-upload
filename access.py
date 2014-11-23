dca614dcb5ef2304ca4cf15f7797765d4b4e94ba
Niclas Snellman <nici@bambuser.com>
2014-11-23 20:49:24 +0100

    Initial version

---
+++ access.py
@@ -0,0 +1,95 @@
+import json
+import urlparse, urllib
+import argparse
+import requests
+import threading
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+REQUEST_TOKEN_URL = "https://www.strava.com/oauth/authorize"
+ACCESS_TOKEN_URL = "https://www.strava.com/oauth/token"
+
+client_id = None
+client_secret_key = None
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        global client_id
+        global client_secret_key
+        request = urlparse.urlparse(self.path)
+        qs = urlparse.parse_qs(request.query)
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        # Exchange a request code for an access token
+        if qs['code']:
+            code = qs['code'][0]
+            params = {
+                'client_id': client_id,
+                'client_secret': client_secret_key,
+                'code': code
+            }
+            r = requests.post(ACCESS_TOKEN_URL, params=params)
+            response = r.text
+            try:
+                response = json.loads(response)
+            except: pass
+
+            if 'access_token' in response:
+                print 'Access token: %s' % response['access_token']
+                self.wfile.write('Access token: %s<br /><br />Copy the access token and supply it when running strava.py' % response['access_token'])
+            else:
+                self.wfile.write("Failed to get access token.")
+
+            killer = threading.Thread(target=http_server.shutdown)
+            killer.daemon = True
+            killer.start()
+
+        self.wfile.close()
+
+    def log_message(self, format, *args):
+        return
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Acquire an access token for Strava API.')
+    parser.add_argument('--client_id', dest='client_id')
+    parser.add_argument('--secret_key', dest='secret_key')
+    parser.add_argument('--port', dest='port', default=15642)
+    args = parser.parse_args()
+
+    if not args.client_id:
+        print "client_id not specified"
+        exit()
+
+    if not args.secret_key:
+        print "secret_key not specified"
+        exit()
+
+    client_id = args.client_id
+    client_secret_key = args.secret_key
+
+    try:
+        args.port = int(args.port)
+    except: pass
+
+    if not isinstance(args.port, int):
+        print "port must be integer"
+        exit()
+
+    params = {
+        'client_id': args.client_id,
+        'response_type': 'code',
+        'redirect_uri': 'http://localhost:' + str(args.port),
+        'scope': 'write'
+    }
+    request_token_url = REQUEST_TOKEN_URL + "?" + urllib.urlencode(params)
+
+    print "Visit the following URL and follow the instructions: %s" % request_token_url
+
+    http_server = HTTPServer(('0.0.0.0', args.port), RequestHandler)
+    try:
+        http_server.serve_forever()
+
+    except KeyboardInterrupt: pass
+    http_server.server_close()
---
+++ strava.py
@@ -0,0 +1,64 @@
+import json, os
+import argparse
+import requests
+
+UPLOAD_URL = "https://www.strava.com/api/v3/uploads"
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Upload activities to Strava.')
+    parser.add_argument("access_token", metavar="ACCESS_TOKEN")
+    parser.add_argument("filepath", metavar="FILE")
+    parser.add_argument('--activity', dest='activity_type', default='run', help='Activity type. Possible values: possible values: ride, run, swim, workout, hike, walk, nordicski, alpineski, backcountryski, iceskate, inlineskate, kitesurf, rollerski, windsurf, workout, snowboard, snowshoe. Default: run.')
+    parser.add_argument('--type', dest='data_type', default=None, help='Data type. Strava supports FIT, TCX and GPX. Default: use file extension.')
+    parser.add_argument('--name', dest='name', default=None, help='Activity name. Defaults to a combination of position and date.')
+    parser.add_argument('--desc', dest='description', default=None, help='Activity description.')
+    args = parser.parse_args()
+
+    filepath = args.filepath
+    if not os.path.isfile(filepath):
+        print "Can't open %s" % filepath
+        exit()
+
+    data_type = args.data_type
+    if not data_type and "." in filepath:
+        data_type = os.path.splitext(filepath)[1][1:]
+    if not data_type:
+        print "Missing data type"
+        exit()
+    data_types = ['fit', 'tcx', 'gpx']
+    if data_type.lower() not in data_types:
+        print "Unsupported data type. Expected one of: %s" % ", ".join(data_types)
+        exit()
+
+    params = {
+        'activity_type': args.activity_type,
+        'data_type': data_type.lower()
+        #'private'
+    }
+    if args.name: params['name'] = args.name
+    if args.description: params['description'] = args.description
+
+    r = requests.post(UPLOAD_URL, params=params,
+    files={
+        'file': open(filepath, 'rb')
+    }, headers={
+        'Authorization': 'Bearer %s' % args.access_token
+    })
+    response = r.text
+
+    try:
+        response = json.loads(response)
+    except: pass
+
+    if r.status_code not in [200,201]:
+        if 'error' in response and response['error']:
+            print "Error: %s" % response['error']
+        else:
+            print "Error: Failed to upload %s" % filepath
+        exit()
+
+    if 'status' in response:
+        print "Strava: %s" % response['status']
+        exit()
+
+    print "Uploaded activity."
